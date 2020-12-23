871a197679ca995c4059ddbb11664d6c2d609c03
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-12-23 14:00:56 +0900

    firebase authenticated users into firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,31 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  return userRef
+}
+
 const config = {
   apiKey: 'AIzaSyDWEAF8mYnkk2tOtYHRrl1tGHcE8n9xKso',
   authDomain: 'crwn-db-2e2cf.firebaseapp.com',
